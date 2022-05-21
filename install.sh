43bd0b32c01cfe1d1c79e9f3212a0ccd074bf9e3
MelzEn <97205073+melsen888@users.noreply.github.com>
2022-05-21 15:07:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,19 @@
+echo "install"
 
+sleep 2
+
+clear
+
+sudo apt install npm
+npm install express@4
+sudo apt install nodejs
+
+echo "============="
+echo "node index.js"
+echo "============="
+
+sleep 2
+
+clear
+
+node index.js
