e5b041802ca708f6b13e6ba3925b37550144488a
MelzEn <97205073+melsen888@users.noreply.github.com>
2022-10-07 22:31:24 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -7,36 +7,7 @@ const http = require('http').Server(express);
 const io = require('socket.io')(http);
 const port = process.env.port || 3000;
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
-});
 
-app.get('/tikus', function (req, res) => {
-  res.sendFile(__dirname,'/pukul-tikus-tanah/index.html');
-});
-
-app.get('/melsen', function (req, res) {
-  res.sendFile(path.join(__dirname,'/public/page/melsen.html'));
-});
-
-app.get('/global', function (req, res) {
-  res.sendFile(path.join(__dirname,'/public/page/global.html'));
-});
-
-app.get('/socket.io', function (req,res){
-  res.sendFile(path.join(__dirname,'/node_modules/socket.io/client-dist/socket.io.js',));
-});
-
-app.get('/login',function (req,res){
-  res.sendFile(path.join(__dirname,'backend/login/admin.js'));
-});
-
-app.get('/barcode',function (req,res){
-  res.sendFile(path.join(__dirname,'/node_modules/jsbarcode'));
-});
-
-app.use(express.static(__dirname + '/public')); // public directory,css,js
-app.use('/game', express.static('pukul-tikus-tanah'))
 app.listen(port);
 
 io.on('connection', (socket) => {
--- index.js
+++ index.js
@@ -16,6 +16,33 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/profile./index.html');
 });
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/melsen', function (req, res) {
+  res.sendFile(path.join(__dirname,'/public/page/melsen.html'));
+});
+
+app.get('/global', function (req, res) {
+  res.sendFile(path.join(__dirname,'/public/page/global.html'));
+});
+
+app.get('/socket.io', function (req,res){
+  res.sendFile(path.join(__dirname,'/node_modules/socket.io/client-dist/socket.io.js',));
+});
+
+app.get('/login',function (req,res){
+  res.sendFile(path.join(__dirname,'backend/login/admin.js'));
+});
+
+app.get('/barcode',function (req,res){
+  res.sendFile(path.join(__dirname,'/node_modules/jsbarcode'));
+});
+
+app.use(express.static(__dirname + '/public')); // public directory,css,js
+app.use('/game', express.static('pukul-tikus-tanah'))
+
 app.use('/public', express.static('public'))
 
 io.on('connection', (socket) => {
